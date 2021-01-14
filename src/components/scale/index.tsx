20154dbfd8e54c134f415dca7c1f21608a13f18b
phl3bas <phl3bas87@gmail.com>
2021-01-14 10:44:01 +0000

    docs: added scale examples

---
+++ src/components/scale/index.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Size } from "./scale";
+
+export const Scale = () => {
+  return (
+    <div style={{ padding: "0 var(--tl-size-12)" }}>
+      <table>
+        <thead>
+          <tr>
+            <th>Token</th>
+            <th>Size</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.keys(Size.size)
+            .sort((a, b) =>
+              a.localeCompare(b, undefined, {
+                numeric: true,
+                sensitivity: "base",
+              })
+            )
+            .map((token) => (
+              <tr>
+                <td>--tl-size-{token}</td>
+                <td>{Size.size[token]}</td>
+                <td style={{ display: "flex" }}>
+                  <div
+                    style={{
+                      background: "var(--tl-teal-400)",
+                      display: "flex",
+                      height: "var(--tl-size-13)",
+                      width: `var(--tl-size-${token})`,
+                      borderRadius: "var(--tl-radius-sm)",
+                    }}
+                  ></div>
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
